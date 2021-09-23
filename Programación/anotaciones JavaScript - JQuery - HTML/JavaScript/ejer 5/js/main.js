2f6135cd95e6a3bcb50495f602162a23d92fd97a
AryDegtiar <adegtiar@frba.utn.edu.ar>
2021-09-23 18:53:12 -0300

    reparacion celuular

--- Programación/anotaciones JavaScript - JQuery - HTML/JavaScript/ejer 5/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict' //modo estricto, te muestra cuando tenes un error sin q lo corrija java y da mas funciones
-
-var num1 = prompt("Ingrese un numero");
-
-for(var i=1; i<=num1; i++){
-	if(num1%i == 0){
-		document.write(" " + i + " ");
-	}
-}
