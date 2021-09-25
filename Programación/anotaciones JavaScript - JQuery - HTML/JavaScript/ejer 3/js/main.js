fc0ebcb85cf31af3c12237482668422ced949c37
AryDegtiar <adegtiar@frba.utn.edu.ar>
2021-09-24 23:54:16 -0300

    v

--- Programación/anotaciones JavaScript - JQuery - HTML/JavaScript/ejer 3/js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict' //modo estricto, te muestra cuando tenes un error sin q lo corrija java y da mas funciones
-
-var num1 = prompt("Ingrese numero 1");
-var num2 = prompt("Ingrese numero 2");
-
-for(var i=(Number(num1)+1); i<num2; i++){
-	document.write(" " + i + " ");
-}
